3d4f522aa25a45d87c09ec0df22dce72115a3096
CTORRES1997A <42396481+CTORRES1997A@users.noreply.github.com>
2019-02-03 16:02:44 -0600

    ACTIVIDAD1.js
    Se hace la actividad 1 ejercicio 1

---
+++ ACTIVIDAD1.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(220);
+	rect(0,0,100, 299);
+	fill(0,0,200);
+	rect(100,0,100, 299);
+	fill(200,0,0);
+	rect(200,0,99, 299);
+	fill(0,200,0);
+}
